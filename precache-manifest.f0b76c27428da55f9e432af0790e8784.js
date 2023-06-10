51c561e83b968b09591c7bc5357e9ef44a630d25
Tanya <tjain5966@gmail.com>
2023-06-10 14:15:03 +0530

    Updates

---
+++ precache-manifest.f0b76c27428da55f9e432af0790e8784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcef8ce1a65989eafc5ba4c5051ef92",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "3c1ed4a95f707bf7336f",
+    "url": "/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expense-tracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1ed4a95f707bf7336f",
+    "url": "/expense-tracker/static/js/main.8b692f00.chunk.js"
+  },
+  {
+    "revision": "7284d3afe7f27382a2ae",
+    "url": "/expense-tracker/static/js/runtime-main.ace402c5.js"
+  }
+]);
